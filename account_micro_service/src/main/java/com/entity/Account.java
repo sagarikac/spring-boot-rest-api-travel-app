2e3be5cd7b5eb18b9e0e02e7d960f360c36365bd
sagarika Choudhury <sagarikac94@gmail.com>
2024-10-12 22:07:53 +0530

    added microservice project

---
+++ account_micro_service/src/main/java/com/entity/Account.java
@@ -0,0 +1,21 @@
+package com.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Data
+//@Setter
+//@Getter
+public class Account {
+@Id
+private int accno;
+private String name;
+private float amount;
+@Column(unique = true)
+private String emailid;
+}
